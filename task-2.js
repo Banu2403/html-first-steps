fed5e4315ec6921bacd7c10e348bebeb062b400f
Banu Isgandarlı <banuisgenderli2011@gmail.com>
2025-12-14 19:26:41 +0400

    Delete task-2.js

--- task-2.js
+++
@@ -1,7 +0,0 @@
-let name = prompt("Write our name");
-let age = prompt("Write your age");
-let hobbies = "";
-if (confirm("Do you want to write ur hobies?")) {
-    hobbies = prompt("Write your hobbies");
-}
-alert(`Name: ${name}\nAge: ${age}\nHobbies: ${hobbies}`);
